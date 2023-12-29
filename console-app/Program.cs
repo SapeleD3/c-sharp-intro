0712ca0696f7d6e0fed86e13bfde49c14fe8d686
moses <ghost@moses.com>
2023-12-29 17:26:09 +0100

    working with numbers

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -1,3 +1,33 @@
 ﻿// Console.Writeline is used for logging to the console
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hi welcome to the Moses show");
+
+// Variables
+string name = "Sapele";
+Console.WriteLine(name);
+
+// string concatination
+string firstName = "Moses";
+string lastName = "Sapele";
+Console.WriteLine("Hello " + firstName + " " + lastName);
+
+// string interpolation
+Console.WriteLine($"Hello {firstName} {lastName}");
+Console.WriteLine($"first name has {firstName.Length} letters while last name {lastName.Length} letters");
+
+// set string to upperCase ToUpper
+// set string to lowerCase ToLower
+// trimming string use the metho Trim() or TrimStart() and TrimEnd()
+// Contains() StartsWith() EndsWith() searches string and returns boolean
+
+int a = 10;
+int b = 12;
+int c = a + b;
+Console.WriteLine(c);
+
+// Numbers int double short long decimal
+
+// the area of a circle
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
